e490e1d0c23c7d5a654e924a83d09595a4afe3d3
Ricardo Tavares <ricardo.tavares@koerber.digital>
2020-07-16 17:58:09 +0100

    feat: Add task components to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" src="../assets/logo.png" class="home__vue-logo" />
+    <h1>Task List</h1>
+
+    <create-task />
+    <task-list />
   </div>
 </template>
 
 <script>
+import TaskList from '@/components/TaskList.vue';
+import CreateTask from '@/components/CreateTask.vue';
+
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    TaskList,
+    CreateTask,
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.home__vue-logo {
+  height: 100px;
+}
+</style>
