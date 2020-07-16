e2a54fd15f7562b6fcc7a7a0c7fb4e610e2ea50d
Ricardo Tavares <ricardo.tavares@koerber.digital>
2020-07-16 10:12:29 +0100

    feat: Add an azure function that returns a random task

---
+++ api/randomTask/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable func-names */
+
+const tasks = [
+  'Clean the kitchen',
+  'Buy light bulbs',
+  'Charge the camera batteries',
+  'Take the parrot to the vet',
+  'Clean the living room',
+  'Take out the trash',
+  'Write more tasks',
+  'Have fun',
+];
+
+module.exports = async function(context) {
+  context.res = {
+    // return a random task
+    body: tasks[Math.floor(Math.random() * tasks.length)],
+  };
+};
