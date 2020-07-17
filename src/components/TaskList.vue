c26719631fe476896659b14d97eea2cf5545b147
Ricardo Tavares <ricardo.tavares@koerber.digital>
2020-07-17 15:32:36 +0100

    feat: Add delete button to tasks and clean up the design

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="task-list">
     <div v-for="task in taskList" :key="task.id" class="task-list__task">
-      {{ task.task }}
+      <div class="task-list__task-description">
+        {{ task.task }}
+      </div>
+
+      <button
+        class="app-btn task-list__delete-task-btn app-btn--small"
+        @click="deleteTask(task.id)"
+      >
+        Delete
+      </button>
     </div>
   </div>
 </template>
@@ -15,7 +24,35 @@ export default {
   computed: {
     ...mapGetters('tasks', ['taskList']),
   },
+
+  methods: {
+    deleteTask(taskId) {
+      this.$store.commit('tasks/DELETE_TASK', taskId);
+    },
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.task-list {
+  text-align: left;
+  width: 700px;
+  margin: 0 auto;
+  margin-top: 10px;
+}
+
+.task-list__task {
+  padding: 10px;
+  display: flex;
+  align-content: center;
+  align-items: center;
+
+  &:hover {
+    background-color: #afe3cc;
+  }
+}
+
+.task-list__task-description {
+  flex: 1;
+}
+</style>
--- tests/unit/components/TaskList.spec.js
+++ tests/unit/components/TaskList.spec.js
@@ -8,6 +8,7 @@ localVue.use(Vuex);
 
 let getters;
 let store;
+let mutations;
 
 beforeEach(() => {
   getters = {
@@ -17,11 +18,16 @@ beforeEach(() => {
     ],
   };
 
+  mutations = {
+    DELETE_TASK: jest.fn(),
+  };
+
   store = new Vuex.Store({
     modules: {
       tasks: {
         namespaced: true,
         getters,
+        mutations,
       },
     },
   });
@@ -33,6 +39,25 @@ describe('TaskList.vue', () => {
 
     expect(wrapper.findAll('.task-list__task')).toHaveLength(
       getters.taskList().length,
+    );
+  });
+
+  it('renders a delete button for each item', () => {
+    const wrapper = shallowMount(TaskList, { store, localVue });
+
+    expect(wrapper.findAll('.task-list__delete-task-btn')).toHaveLength(
+      getters.taskList().length,
+    );
+  });
+
+  it('commits the delete mutation with the task id when its delete button is clicked', () => {
+    const wrapper = shallowMount(TaskList, { store, localVue });
+
+    wrapper.find('.task-list__delete-task-btn').trigger('click');
+
+    expect(mutations.DELETE_TASK).toHaveBeenCalledWith(
+      expect.anything(),
+      getters.taskList()[0].id,
     );
   });
 });
